12dc7b742f76f5746bd2cd6eb3e77c35b49bd7ff
Vanya Sergeev <vsergeev@gmail.com>
2016-07-03 01:30:08 -0700

    blocks/protocol/varicodedecode: add VaricodeDecoderBlock

--- radio/blocks/init.lua
+++ radio/blocks/init.lua
@@ -90,4 +90,6 @@ return {
     --- POCSAG
     POCSAGFramerBlock = require('radio.blocks.protocol.pocsagframer'),
     POCSAGDecoderBlock = require('radio.blocks.protocol.pocsagdecoder'),
+    --- Varicode
+    VaricodeDecoderBlock = require('radio.blocks.protocol.varicodedecoder'),
 }
